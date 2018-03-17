c4f731e00cdbf520360429ea10e1f4e88d047a6d
Olly Warren <bunoire14@icloud.com>
2018-03-17 21:47:06 +0000

    few tweaks

--- src/config/prestashop.php
+++ src/config/prestashop.php
@@ -3,4 +3,5 @@ return [
     // Required Settings
     'url'                  => env('PRESTASHOP_URL', 'https://github.com'),
     'key'                  => env('PRESTASHOP_KEY', 'ABC123'),
+    'debug'                => env('PRESTASHOP_DEBUG', false),
 ];
--- src/services/PrestashopService.php
+++ src/services/PrestashopService.php
@@ -27,6 +27,7 @@ class PrestashopService
         // Required Settings
 		$this->url               = config('prestashop.url');
         $this->consumer_key      = config('prestashop.key');
+        $this->debug             = config('prestashop.debug');
         $this->client            = new \PrestaShopWebservice($this->url, $this->key);
 	}
 
